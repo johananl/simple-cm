b8552bd9db4c6995d69ce4c9cec03312439ae534
Johanan Liebermann <johanan.liebermann@gmail.com>
2018-05-15 03:39:00 +0200

    Test multiple hosts

--- cmd/master/main.go
+++ cmd/master/main.go
@@ -6,6 +6,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/rpc"
+	"os"
+	"sync"
 
 	ops "github.com/johananl/simple-cm/operations"
 	"github.com/johananl/simple-cm/worker"
@@ -26,7 +28,7 @@ func main() {
 	gob.Register(ops.FileContainsOperation{})
 
 	// Read SSH private key
-	key, err := ioutil.ReadFile("./private_key")
+	key, err := ioutil.ReadFile(os.Getenv("SSH_KEY"))
 	if err != nil {
 		log.Fatalf("error reading SSH key: %v", err)
 	}
@@ -57,64 +59,70 @@ func main() {
 		log.Fatalf("error dialing: %v", err)
 	}
 
+	var wg sync.WaitGroup
 	for _, h := range hosts {
-		o := []ops.Operation{
-			ops.FileExistsOperation{
-				Description: "verify_test_file_exists",
-				Path:        "/tmp/test.txt",
-			},
-			ops.FileContainsOperation{
-				Description: "verify_test_file_contains_hello",
-				Path:        "/tmp/test.txt",
-				Text:        "hello",
-			},
-		}
-
-		in := worker.ExecuteInput{
-			Host:       h,
-			Operations: o,
-		}
-		var out worker.ExecuteOutput
-		err = client.Call("Worker.Execute", in, &out)
-		if err != nil {
-			log.Printf("error executing operations: %v", err)
-		}
-
-		// Analyze results
-		var good, bad []ops.OperationResult
-		for _, i := range out.Results {
-			if i.Successful {
-				good = append(good, i)
-			} else {
-				bad = append(bad, i)
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			o := []ops.Operation{
+				ops.FileExistsOperation{
+					Description: "verify_test_file_exists",
+					Path:        "/tmp/test.txt",
+				},
+				ops.FileContainsOperation{
+					Description: "verify_test_file_contains_hello",
+					Path:        "/tmp/test.txt",
+					Text:        "hello",
+				},
 			}
-		}
 
-		// TODO Set colors for success / fail
-		if len(good) > 0 {
-			log.Println("Completed operations:")
-			for _, i := range good {
-				fmt.Println("* ", i.Operation.Desc())
-				if i.StdOut != "" {
-					fmt.Printf("stdout:\n%v", formatScriptOutput(i.StdOut))
-				}
-				if i.StdErr != "" {
-					fmt.Printf("stderr:\n%v", formatScriptOutput(i.StdErr))
+			in := worker.ExecuteInput{
+				Host:       h,
+				Operations: o,
+			}
+			var out worker.ExecuteOutput
+			err = client.Call("Worker.Execute", in, &out)
+			if err != nil {
+				log.Printf("error executing operations: %v", err)
+			}
+
+			// Analyze results
+			var good, bad []ops.OperationResult
+			for _, i := range out.Results {
+				if i.Successful {
+					good = append(good, i)
+				} else {
+					bad = append(bad, i)
 				}
 			}
-		}
 
-		if len(bad) > 0 {
-			log.Println("Failed operations:")
-			for _, i := range bad {
-				fmt.Println("* ", i.Operation.Desc())
-				if i.StdOut != "" {
-					fmt.Printf("stdout:\n%v", formatScriptOutput(i.StdOut))
-				}
-				if i.StdErr != "" {
-					fmt.Printf("stderr:\n%v", formatScriptOutput(i.StdErr))
+			// TODO Set colors for success / fail
+			if len(good) > 0 {
+				log.Println("Completed operations:")
+				for _, i := range good {
+					fmt.Println("* ", i.Operation.Desc())
+					if i.StdOut != "" {
+						fmt.Printf("stdout:\n%v", formatScriptOutput(i.StdOut))
+					}
+					if i.StdErr != "" {
+						fmt.Printf("stderr:\n%v", formatScriptOutput(i.StdErr))
+					}
 				}
 			}
-		}
+
+			if len(bad) > 0 {
+				log.Println("Failed operations:")
+				for _, i := range bad {
+					fmt.Println("* ", i.Operation.Desc())
+					if i.StdOut != "" {
+						fmt.Printf("stdout:\n%v", formatScriptOutput(i.StdOut))
+					}
+					if i.StdErr != "" {
+						fmt.Printf("stderr:\n%v", formatScriptOutput(i.StdErr))
+					}
+				}
+			}
+		}()
+		wg.Wait()
 	}
 }
