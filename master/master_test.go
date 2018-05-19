7077dd106cd5c34e946e7019cb2595f57e83d16c
Johanan Liebermann <johanan.liebermann@gmail.com>
2018-05-19 19:58:00 +0200

    Add test for SelectWorker

--- master/master_test.go
+++ master/master_test.go
@@ -2,13 +2,15 @@ package master
 
 import (
 	"io/ioutil"
+	"net/rpc"
 	"os"
 	"testing"
 )
 
 func TestSSHKey(t *testing.T) {
-	m := Master{SSHKeysPath: "./"}
+	m := Master{SSHKeysDir: "./"}
 
+	// TODO Generate a real SSH key
 	fn := "./test_key"
 	contents := "secretstuff"
 	err := ioutil.WriteFile(fn, []byte(contents), 0644)
@@ -29,5 +31,33 @@ func TestSSHKey(t *testing.T) {
 
 	if k != contents {
 		t.Fatalf("wrong contents read from key: got %v want %v", k, contents)
+	}
+}
+
+func TestSelectWorker(t *testing.T) {
+	w := []*rpc.Client{
+		&rpc.Client{},
+		&rpc.Client{},
+		&rpc.Client{},
+		&rpc.Client{},
+		&rpc.Client{},
+	}
+	m := Master{
+		Workers:        w,
+		LastUsedWorker: 0,
+	}
+
+	var want int
+	for i := 0; i < len(w); i++ {
+		if want == len(w)-1 {
+			want = 0
+		} else {
+			want = i + 1
+		}
+
+		c := m.SelectWorker()
+		if c != m.Workers[want] {
+			t.Fatalf("Wrong worker selected: got %v want %v", &c, &m.Workers[want])
+		}
 	}
 }
